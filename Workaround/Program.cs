5b4ad016978a0b86bbc663f59f50957b6e5d8468
Omer Kazkayasi <omer.kazkayasi@functionair.com>
2024-02-06 16:29:18 +0300

    Proje dosyası ekle.

---
+++ Workaround/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Workaround;
+
+Console.WriteLine("Hello, World!");
+string mesaj = "Merhaba";
+double tutar = 10000;//dbden gelir
+int sayi = 100;
+
+Vatandas vatandas = new Vatandas();
---
+++ Workaround/Vatandas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workaround
+{
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+}
